7d79ba8ea1439dfb49722a939f8aa46875905011
crkur <ckurdelak20@georgefox.edu>
2023-04-21 10:53:40 -0700

    added password to class server

--- database/Database.php
+++ database/Database.php
@@ -5,7 +5,8 @@ class Database
     function connect(): PDO
     {
         try {
-            $db = new PDO("pgsql:dbname=blog_db;host=localhost;password=314dev;user=dev");
+            //$db = new PDO("pgsql:dbname=blog_db;host=localhost;password=314dev;user=dev");
+            $db = new PDO("pgsql:dbname=blog_db;host=localhost;password=ckurdelak20;user=ckurdelak20");
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (Exception $e) {
             exit('blog-db\database\Database Connection Error! ' . $e->getMessage());
